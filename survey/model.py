561828659836627c9c44fae468c27048f7aada8a
ER LUNKO <lunko@webaps.com>
2020-12-13 00:09:59 +0100

    model actualizado

--- survey/model.py
+++ survey/model.py
@@ -4,7 +4,7 @@ from . import db
 
 # UserMixin will add Flask login attributes to the model
 class User(UserMixin, db.Model):
-    __tablename__ = "user"
+    # __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True, nullable=False)
     name = db.Column(db.String(128), nullable=False)
@@ -14,12 +14,9 @@ class User(UserMixin, db.Model):
         backref="user",
         lazy=True,  # select
         cascade="all, delete-orphan",
-        # order_by="Survey.timestamp"
+        order_by="Survey.timestamp"
     )
-    # messages = db.relationship(
-    #     'Message',
-    #     backref='user',
-    #     lazy=True)
+
 
 # class Message(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
@@ -35,7 +32,7 @@ class Survey(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     owner_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     title = db.Column(db.String(64), nullable=False)
-    description = db.Column(db.String(128), nullable=True)
+    description = db.Column(db.String(512), nullable=True)
     state = db.Column(db.Enum(SurveyState), nullable=False)
     timestamp = db.Column(db.DateTime(), nullable=False)
     questions = db.relationship(
@@ -43,13 +40,30 @@ class Survey(db.Model):
         backref="survey",
         lazy=True,
         cascade="all, delete-orphan",
-        # order_by="Question.position"
+        order_by="Question.position"
     )
+
+
+class QuestionType(enum.Enum):
+    OneAnswer = 1
+    ManyAnswers = 2
+    TextAnswer = 3
+    NumberAnswer = 4
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     survey_id = db.Column(db.Integer,db.ForeignKey("survey.id"), nullable=False)
-    # ...
+    statement = db.Column(db.String(1024), nullable=False)
+    type = db.Column(db.Enum(QuestionType), nullable=False)
+    position = db.Column(db.Integer, nullable=False)
+    options = db.relationship(
+        "QuestionOption",
+        backref="option",
+        lazy=True,
+        cascade="all, delete-orphan",
+        # order_by= ??
+    )
+    
 
 class QuestionOption(db.Model):
     id = db.Column(db.Integer, primary_key=True)
